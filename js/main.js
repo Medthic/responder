4011f68ea36f79ac4c65f88ed82fecedc5301cde
Medthic <114042009+Medthic@users.noreply.github.com>
2024-03-26 21:40:09 -0400

    1

--- js/main.js
+++ js/main.js
@@ -1,47 +1,51 @@
 function showTime() {
-    var date = new Date();
-    var h = date.getHours(); // 0 - 23
-    var m = date.getMinutes(); // 0 - 59
-    var s = date.getSeconds(); // 0 - 59
+  var date = new Date()
+  var h = date.getHours() // 0 - 23
+  var m = date.getMinutes() // 0 - 59
+  var s = date.getSeconds() // 0 - 59
 
-    h = (h < 10) ? "0" + h : h;
-    m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : s;
+  h = h < 10 ? "0" + h : h
+  m = m < 10 ? "0" + m : m
+  s = s < 10 ? "0" + s : s
 
-    var time = h + ":" + m + ":" + s;
-    document.getElementById("Clock").innerText = time;
-    document.getElementById("Clock").textContent = time;
+  var time = h + ":" + m + ":" + s
+  document.getElementById("Clock").innerText = time
+  document.getElementById("Clock").textContent = time
 
-    setTimeout(showTime, 1000);
-
+  setTimeout(showTime, 1000)
 }
 
-showTime();
+showTime()
 
 function getWeatherForecast() {
-    fetch('https://api.weather.gov/gridpoints/PBZ/85,67/forecast')
-        .then(response => response.json())
-        .then(data => {
-            const tonightForecast = data.properties.periods.find(period => period.name === 'Overnight');
-            if (tonightForecast) {
-                document.getElementById('temperature').textContent = tonightForecast.temperature + '°F';
+  fetch("https://api.weather.gov/gridpoints/PBZ/85,67/forecast")
+    .then((response) => response.json())
+    .then((data) => {
+      const tonightForecast = data.properties.periods.find(
+        (period) => period.name === "Overnight"
+      )
+      if (tonightForecast) {
+        document.getElementById("temperature").textContent =
+          tonightForecast.temperature + "°F"
 
-                const temperature = parseInt(tonightForecast.temperature);
-                if (temperature < 32) {
-                    document.getElementById('temperature').style.color = '#007acc'; // Cold temperature
-                } else if (temperature > 80) {
-                    document.getElementById('temperature').style.color = '#ff7858'; // Hot temperature
-                } else {
-                    document.getElementById('temperature').style.color = '#adbcc1'; // Moderate temperature
-                }
-            } else {
-                document.getElementById('temperature').textContent = 'Failed to fetch weather data.';
-            }
-        })
-        .catch(error => {
-            console.error('Error fetching weather data:', error);
-            document.getElementById('temperature').textContent = 'Failed to fetch weather data.';
-        });
+        const temperature = parseInt(tonightForecast.temperature)
+        if (temperature < 32) {
+          document.getElementById("temperature").style.color = "#007acc" // Cold temperature
+        } else if (temperature > 80) {
+          document.getElementById("temperature").style.color = "#ff7858" // Hot temperature
+        } else {
+          document.getElementById("temperature").style.color = "#adbcc1" // Moderate temperature
+        }
+      } else {
+        document.getElementById("temperature").textContent =
+          "Failed to fetch weather data."
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching weather data:", error)
+      document.getElementById("temperature").textContent =
+        "Failed to fetch weather data."
+    })
 }
 
-window.onload = getWeatherForecast;
+window.onload = getWeatherForecast
