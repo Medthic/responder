e4239c34514aebf93fd45d30f6a0691018cf3645
Medthic <114042009+Medthic@users.noreply.github.com>
2024-03-28 22:11:24 -0400

    11

--- js/main.js
+++ js/main.js
@@ -1,5 +1,54 @@
+import { initializeApp } from "@firebase/app"
+import { getDatabase } from "@firebase/database"
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCXkNsf5a6xdNNV3KHuxR8sFwBjMwWnQZo",
+  authDomain: "responder-gg.firebaseapp.com",
+  databaseURL: "https://responder-gg-default-rtdb.firebaseio.com",
+  projectId: "responder-gg",
+  storageBucket: "responder-gg.appspot.com",
+  messagingSenderId: "283970004750",
+  appId: "1:283970004750:web:27ad83b13555f5c78a595f",
+  measurementId: "G-96WCVVYHQW",
+}
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+
+// Call function to handle form submission on page load
+handleFormSubmission()
+
+// Function to handle form submission
+function handleFormSubmission() {
+  // Get form data
+  var name = document.getElementById("name").value
+  var rank = document.getElementById("rank").value
+
+  // Push data to Firebase
+  var database = firebase.database()
+  var usersRef = database.ref("users")
+  var newUserRef = usersRef.push()
+  newUserRef
+    .set({
+      name: name,
+      rank: rank,
+      driverStatus: "yes",
+    })
+    .then(function () {
+      console.log("Data successfully written!")
+      // Clear form fields after successful submission
+      document.getElementById("name").value = ""
+      document.getElementById("rank").value = ""
+    })
+    .catch(function (error) {
+      console.error("Error writing data: ", error)
+    })
+}
+
 function showTime() {
-  var date = new Date()
+  const newLocal = new Date()
+  var date = newLocal
   var h = date.getHours() // 0 - 23
   var m = date.getMinutes() // 0 - 59
   var s = date.getSeconds() // 0 - 59
