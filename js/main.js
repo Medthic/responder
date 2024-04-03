82c82ea42c8f73a7a3db758abbf6c93e86c88371
Medthic <114042009+Medthic@users.noreply.github.com>
2024-04-02 22:40:48 -0400

    1.001

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 function showTime() {
-  const newLocal = new Date()
-  var date = newLocal
+  var date = new Date()
   var h = date.getHours() // 0 - 23
   var m = date.getMinutes() // 0 - 59
   var s = date.getSeconds() // 0 - 59
@@ -17,36 +16,3 @@ function showTime() {
 }
 
 showTime()
-
-function getWeatherForecast() {
-  fetch("https://api.weather.gov/gridpoints/PBZ/85,67/forecast")
-    .then((response) => response.json())
-    .then((data) => {
-      const tonightForecast = data.properties.periods.find(
-        (period) => period.name === "Overnight"
-      )
-      if (tonightForecast) {
-        document.getElementById("temperature").textContent =
-          tonightForecast.temperature + "°F"
-
-        const temperature = parseInt(tonightForecast.temperature)
-        if (temperature < 32) {
-          document.getElementById("temperature").style.color = "#007acc" // Cold temperature
-        } else if (temperature > 80) {
-          document.getElementById("temperature").style.color = "#ff7858" // Hot temperature
-        } else {
-          document.getElementById("temperature").style.color = "#adbcc1" // Moderate temperature
-        }
-      } else {
-        document.getElementById("temperature").textContent =
-          "Failed to fetch weather data."
-      }
-    })
-    .catch((error) => {
-      console.error("Error fetching weather data:", error)
-      document.getElementById("temperature").textContent =
-        "Failed to fetch weather data."
-    })
-}
-
-window.onload = getWeatherForecast
